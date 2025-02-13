3ad7a24bd9d465d37423bc4c03eab1ee409a6923
LeFilly75 <redfoo923@gmail.com>
2025-02-13 13:24:01 -0500

    add marche

--- dashboard/models/user_model.py
+++ dashboard/models/user_model.py
@@ -1,11 +1,12 @@
+from sqlalchemy import Integer, String, Column, DateTime
+from sqlalchemy.sql import func
 from models import db
-class User(db.Model):
-  __tablename__= "users"
 
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(100), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(200), nullable=False)
-  role = db.Column(db.String(50), nullable=False)
-  date_created = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=True)
- 
+class User(db.Model):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)  # Clé primaire auto-incrémentée
+    username = Column(String(100), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(200), nullable=False)
+    role = Column(String(50), nullable=False)
+    date_created = Column(DateTime, default=func.current_timestamp(), nullable=True)
